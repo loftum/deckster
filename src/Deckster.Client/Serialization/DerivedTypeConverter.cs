15aec36446f48fe392492b25f8997fa086c3d4c7
Hans Olav <loftum@gmail.com>
2024-10-13 16:09:50 +0200

    JsonNode -> JsonElement

--- src/Deckster.Client/Serialization/DerivedTypeConverter.cs
+++ src/Deckster.Client/Serialization/DerivedTypeConverter.cs
@@ -1,36 +1,11 @@
 using System.Text.Json;
-using System.Text.Json.Nodes;
 using System.Text.Json.Serialization;
 using Deckster.Client.Protocol;
 
 namespace Deckster.Client.Serialization;
 
-public class DecksterMessageConverter : JsonConverterFactory
-{
-    private static readonly Type BaseType = typeof(DecksterMessage);
-    private readonly Dictionary<string, Type> _typeMap;
-
-    public DecksterMessageConverter(Dictionary<string, Type> typeMap)
-    {
-        _typeMap = typeMap;
-    }
-
-    public override bool CanConvert(Type typeToConvert)
-    {
-        return typeToConvert.IsSubclassOf(BaseType) && typeToConvert.IsAbstract;
-    }
-
-    public override JsonConverter? CreateConverter(Type typeToConvert, JsonSerializerOptions options)
-    {
-        var ctor = typeof(DerivedTypeConverter<>).MakeGenericType(typeToConvert).GetConstructors()[0];
-        var instance = ctor.Invoke([_typeMap]);
-        return (JsonConverter) instance;
-    }
-}
-
 public class DerivedTypeConverter<T> : JsonConverter<T> where T : DecksterMessage
 {
-    // ReSharper disable once StaticMemberInGenericType
     private readonly Dictionary<string, Type> _typeMap;
 
     public DerivedTypeConverter(Dictionary<string, Type> typeMap)
@@ -40,8 +15,13 @@ public class DerivedTypeConverter<T> : JsonConverter<T> where T : DecksterMessag
 
     public override T? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
     {
-        var node = JsonNode.Parse(ref reader);
-        var discriminator = node?["type"]?.GetValue<string>();
+        var element = JsonElement.ParseValue(ref reader);
+        if (!element.TryGetProperty("type", out var p) || p.ValueKind != JsonValueKind.String)
+        {
+            return default;
+        }
+
+        var discriminator = p.GetString();
         if (discriminator == null)
         {
             return default;
@@ -49,7 +29,7 @@ public class DerivedTypeConverter<T> : JsonConverter<T> where T : DecksterMessag
 
         if (_typeMap.TryGetValue(discriminator, out var type))
         {
-            return (T?) node.Deserialize(type, options);
+            return (T?) element.Deserialize(type, options);
         }
 
         return default;
